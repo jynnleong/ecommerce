f7d8c527b7a3d431832275a894726515f487be27
jynnleong <ojleong@mun.ca>
2021-05-29 01:23:44 -0400

    Version 1

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const connectDB = require('./connectDB');
 const cors = require('cors');
@@ -23,11 +25,14 @@ app.use("/cart", cartRouter);
 
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, "client", "build")));
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "client", "build")));
+    
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+}
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
 
 
 app.listen(port, () => {
